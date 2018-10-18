92175d28a4e25d8a960f33d957663d88bf8131cd
htcr <congruihetang@gmail.com>
2018-10-17 21:06:21 -0400

    tried animation

--- testCarSequence.py
+++ testCarSequence.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import matplotlib.patches as patches
+from LucasKanade import LucasKanade
 
-# write your script here, we recommend the above libraries for making your animation
---
+++ test_anim.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+class Video(object):
+    def __init__(self, vid_data):
+        # vid_data: np array, [h, w, N_frames]
+        self.vid_data = vid_data
+        self.frame_num = vid_data.shape[2]
+        self.fig, self.ax = plt.subplots()
+        
+    def frame_ids(self):
+        cnt = 0
+        while cnt < self.frame_num:
+            yield cnt
+            cnt += 1
+
+    def draw_frame(self, frame_id):
+        cur_frame = self.vid_data[:, :, frame_id]
+        if frame_id == 0:
+            self.buff = self.ax.imshow(cur_frame, cmap='gray')
+        else:
+            self.buff.set_array(cur_frame)
+        # draw other stuff here
+        
+    def play(self):
+        anim = animation.FuncAnimation(self.fig, self.draw_frame, self.frame_ids, blit=False, interval=5,
+                              repeat=True)
+        plt.show()
+
+
+car_vid = np.load('../data/carseq.npy')
+car_anim = Video(car_vid)
+car_anim.play()
